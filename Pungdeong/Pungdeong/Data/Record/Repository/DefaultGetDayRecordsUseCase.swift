3c937c119b90ede9434c8968f69b9f63b466d98f
sun <sts07190@naver.com>
2026-03-30 16:51:51 +0900

    [feat/#5] Record 데이터 레이어 구현

---
+++ Pungdeong/Pungdeong/Data/Record/Repository/DefaultGetDayRecordsUseCase.swift
@@ -0,0 +1,24 @@
+//
+//  DefaultGetDayRecordsUseCase.swift
+//  Pungdeong
+//
+//  Created by sun on 3/29/26.
+//
+
+import Foundation
+
+final class DefaultGetDayRecordsUseCase: GetDayRecordsUseCase {
+    private let calendar = Calendar.current
+
+    func execute(baseDate: Date) -> [DailyRecord] {
+        let today = calendar.startOfDay(for: baseDate)
+        let yesterday = calendar.date(byAdding: .day, value: -1, to: today) ?? today
+        let beforeYesterday = calendar.date(byAdding: .day, value: -2, to: today) ?? today
+
+        return [
+            DailyRecord(date: beforeYesterday),
+            DailyRecord(date: yesterday),
+            DailyRecord(date: today)
+        ]
+    }
+}
